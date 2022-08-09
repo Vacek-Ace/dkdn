5f9eb59f1ea46c9abc2eab040f6ccf8b239890b8
Vacek-Ace <rodetlas@gmail.com>
2022-08-09 21:25:07 +0200

    sampling heuristic

--- src/experiments/sampling_experiment.py
+++ src/experiments/sampling_experiment.py
@@ -13,52 +13,51 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import ParameterGrid
+
 
 from src.model.sampling import *
-from src.utils import scaled_mcc, NpEncoder
+from src.utils import NpEncoder
 from src.model.dkdn import DkDN
 
 
 for experiment in [
 # 'a9a',
 'appendicitis',
-# 'australian',
-# 'backache',
-# 'banknote',
-# 'breastcancer',
-# 'bupa',
-# 'cleve',
-# 'cod-rna',
-# 'colon-cancer',
-# 'diabetes',
-# 'flare',
-# 'fourclass',
-# 'german_numer',
-# 'haberman',
-# 'heart',
-# 'housevotes84',
-# 'ilpd',
-# 'ionosphere',
-# 'kr_vs_kp',
-# 'liver-disorders',
-# 'mammographic',
-# 'mushroom',
-# 'r2',
-# 'sonar',
-# 'splice',
-# 'svmguide1',
-# 'svmguide3',
-# 'transfusion',
-# 'w1a',
-# 'w2a',
-# 'w3a',
-# 'w4a',
-# 'w5a',
-# 'w6a',
-# 'w7a',
-# 'w8a'
+'australian',
+'backache',
+'banknote',
+'breastcancer',
+'bupa',
+'cleve',
+'cod-rna',
+'colon-cancer',
+'diabetes',
+'flare',
+'fourclass',
+'german_numer',
+'haberman',
+'heart',
+'housevotes84',
+'ilpd',
+'ionosphere',
+'kr_vs_kp',
+'liver-disorders',
+'mammographic',
+'mushroom',
+'r2',
+'sonar',
+'splice',
+'svmguide1',
+'svmguide3',
+'transfusion',
+'w1a',
+'w2a',
+'w3a',
+'w4a',
+'w5a',
+'w6a',
+'w7a',
+'w8a'
 ]:
     
     print(f'Experiment: {experiment}\n')
@@ -89,8 +88,10 @@ for experiment in [
     complexity_d_class_0 = np.mean(complexity_d[mask_class_0])
     complexity_d_class_1 = np.mean(complexity_d[mask_class_1])
     
-    rng_seed = 1234
-    
+    minority_class_idx, minority_class_proportion = minority_class_properties(y_train)
+
+    if minority_class_proportion > 0.10:
+        minority_class_idx = []
     
     exp_info = {experiment: {'info': 
         {'complexity': {'global': [complexity_d_global],
@@ -102,60 +103,82 @@ for experiment in [
                 'n1': len(y_train[mask_class_1])}
         }}}
     
-    print(exp_info, '\n')
-   
-    methods = [[SVC, {'C': [1, 10, 100, 1000], 'gamma': [0.0001, 0.001, 0.01, 0.1, 1, 10], 'kernel':['rbf'], 
-                      'random_state': [rng_seed]}],
-            [KNeighborsClassifier, {'n_neighbors' : list(range(1, 13, 2)), 'n_jobs': [-1]}],
-            [RandomForestClassifier, {'max_features': [None, 'sqrt', 'log2'], 'n_estimators': [100, 300, 500], 'n_jobs': [-1], 
-                                      'random_state': [rng_seed]}],
-            [GradientBoostingClassifier, {'max_features': [None, 'sqrt', 'log2'], 'max_depth': [3, 5, 7], 
-                                            'learning_rate': [0.1, 0.2, 0.3], 'n_estimators': [100, 300, 500], 'random_state': [rng_seed]}]
-    ]
-    
-    cuts = [round(i*0.01, 2) for i in range(5, 100, 5)]
-    complexity_grouped = complexity_grouping(complexity_d, cuts)
-    rng_cuts = [[0.05, 0.10, 0.15, 0.20, 0.25], [0.30, 0.40, 0.45], [0.50, 0.60, 0.70, 0.75], [0.80, 0.85], [0.90, 0.95]]
+    # random instance
+    rng_seed = 1234
     rng = np.random.default_rng(rng_seed)
-    smpl_cuts = [rng.choice(i) for i in rng_cuts]
-    samples_filtered = filter_sampling(complexity_grouped, cuts=smpl_cuts, p=1, random_state=1234)
+
+    # complexity threshold-cuts
+    cuts = [round(i*0.01, 2) for i in range(5, 100, 5)]
+
+    # grouping complexity according to the threshold-cuts
+    complexity_grouped = complexity_grouping(complexity_d, cuts)
     
-    skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=rng_seed)
-    
+    print(exp_info, '\n')
+
+    methods = [[SVC, {'C': [1, 10, 100, 1000], 'gamma': [0.0001, 0.001, 0.01, 0.1, 1, 10], 'kernel':['rbf'], 
+                        'random_state': [rng_seed]}],
+                [KNeighborsClassifier, {'n_neighbors' : list(range(1, 13, 2)), 'n_jobs': [-1]}],
+                [RandomForestClassifier, {'max_features': [None, 'sqrt', 'log2'], 'n_estimators': [100, 300, 500], 'n_jobs': [-1], 
+                                        'random_state': [rng_seed]}],
+                [GradientBoostingClassifier, {'max_features': [None, 'sqrt', 'log2'], 'max_depth': [3, 5, 7], 
+                                                'learning_rate': [0.1, 0.2, 0.3], 'n_estimators': [100, 300, 500], 'random_state': [rng_seed]}]
+        ]
+
     for method, grid_params in methods:
         print(f'{str(method())[:-2]} \n')
-        for i, complexity_index in enumerate(samples_filtered):
-            best_score = 0
-            for params in list(ParameterGrid(grid_params)):
-                clf = method(**params)
-                scores = []
-                for train_index, test_index in skf.split(X_train, y_train):
-                    try:
-                        sample_index = list(set(train_index).intersection(complexity_index))
-                        clf.fit(X_train[sample_index], y_train[sample_index])
-                        preds = clf.predict(X_train[test_index])
-                        scoring = scaled_mcc(y_train[test_index], preds)
-                    except:
-                        scoring = 0
-                    scores.extend([scoring])
-                if np.mean(scores) > best_score:
-                    best_score = np.mean(scores)
-                    best_params = params
-            if ~(best_score < performance_thresholds[i]):
+        # Grouped cuts based on the experimental distribution
+        rng_cuts = [[0.05, 0.10, 0.15, 0.20, 0.25], [0.30, 0.40, 0.45], [0.50, 0.60, 0.70, 0.75], [0.80, 0.85], [0.90, 0.95]]
+
+        # Initial threshold-cuts: Pick one element from each group of threshold-cuts
+        smpl_cuts = [rng.choice(i) for i in rng_cuts]
+        
+        # Initial best-scores
+        samples_scores = np.full(len(cuts), 0.0)
+        
+        # Initial best-params
+        samples_params = np.full(len(cuts), None)
+        
+        # Initial best-samples
+        samples_idx = np.full(len(cuts), None)
+        
+        for i in range(round((len(cuts) - len(rng_cuts))/2)):
+            
+            if len(minority_class_idx)>0:
+                samples_scores, samples_params, sample_idx = hyperparameter_selection_adjustment(X_train, y_train, smpl_cuts, cuts, method, grid_params, 
+                                                                                  complexity_grouped, samples_scores, samples_params, 
+                                                                                  samples_idx, rng_seed, minority_class_idx)
+            else:
+                samples_scores, samples_params, sample_idx = hyperparameter_selection(X_train, y_train, smpl_cuts, cuts, method, grid_params, 
+                                                                                    complexity_grouped, samples_scores, samples_params, 
+                                                                                    samples_idx, rng_seed)
+            new_idx = search_idx(samples_scores, samples_params, cuts)
+            smpl_cuts = np.array(cuts)[new_idx]
+            
+            if not len(smpl_cuts) > 0:
                 break
-
+        
+        best_index = np.argmax(samples_scores)
+        best_params = samples_params[best_index]
+        best_score = samples_scores[best_index]
+        best_sample = sample_idx[best_index]
+        
         clf = method(**best_params)
-        clf.fit(X_train[complexity_index], y_train[complexity_index])
+        clf.fit(X_train[best_sample], y_train[best_sample])
         preds_test = clf.predict(X_test)
-        score_test = scaled_mcc(y_test, preds_test)
-        print(f'best score: {best_score} \n')
+        test_score = scaled_mcc(y_test, preds_test)
+        
+        print(f'test score: {test_score} \n')
+        print(f'threshold: {cuts[best_index]} \n')
+        print(f'sample_proportion: {round(len(best_sample)/len(X_train), 2)} \n')
+        
         method_info = {
-        'best_score': best_score,
-        'best_params': best_params,
-        'sample_proportion': round(len(complexity_index)/len(X_train), 2),
-        'threshold': complexity_cuts_grouped[complexity_index].max()
-            }
-
+            'test_score': test_score,
+            'best_score': best_score,
+            'best_params': best_params,
+            'sample_proportion': round(len(best_sample)/len(X_train), 2),
+            'threshold': cuts[best_index]
+                }
+        
         exp_info[str(method())[:-2]] = method_info
    
     def get_store_name(experiment, results_folder):
--- src/model/sampling.py
+++ src/model/sampling.py
@@ -1,6 +1,8 @@
 from collections.abc import Iterable
 import numpy as np
-
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import ParameterGrid
+from src.utils import scaled_mcc
 
 def complexity_grouping(complexity, cuts):
     out = np.empty_like(complexity)
@@ -45,3 +47,118 @@ def filter_sampling(complexity, cuts=None, p=1, random_state=1234):
         is_idx[i].extend(samp_idx)
         
     return is_idx
+
+
+def hyperparameter_selection_adjustment(X_train, y_train, smpl_cuts, cuts, method, grid_params, complexity_grouped, samples_scores, samples_params, samples_idx, rng_seed, minority_class_idx):
+
+    skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=rng_seed)
+
+    # Indexes of the picked threshold-cuts
+    smpl_idx = [cuts.index(i) for i in smpl_cuts]    
+    
+    # Corresponding index to each threshold-cut
+    samples_filtered = filter_sampling(complexity_grouped, cuts=smpl_cuts, p=1, random_state=1234)
+    
+    for i, complexity_index in list(zip(smpl_idx, samples_filtered)):
+        
+        best_score = 0
+        best_params = None
+        for params in list(ParameterGrid(grid_params)):
+            
+            clf = method(**params)
+            scores = []
+            
+            for train_index, test_index in skf.split(X_train, y_train):
+            
+                sample_set = set(train_index).intersection(complexity_index)
+                minority_class_active = set(train_index).intersection(minority_class_idx)
+                sample_index = list(sample_set.union(set(minority_class_active)))
+            
+                try:
+                    clf.fit(X_train[sample_index], y_train[sample_index])
+                    preds = clf.predict(X_train[test_index])
+                    scoring = scaled_mcc(y_train[test_index], preds)
+                except:
+                    scoring = 0
+                scores.extend([scoring])
+                
+            if np.mean(scores) > best_score:
+                best_score = np.mean(scores)
+                best_params = params
+                
+        samples_scores[i] = best_score
+        samples_params[i] = best_params
+        samples_idx[i] = list(set(complexity_index).union(set(minority_class_idx)))
+        
+    return samples_scores, samples_params, samples_idx
+
+
+def hyperparameter_selection(X_train, y_train, smpl_cuts, cuts, method, grid_params, complexity_grouped, samples_scores, samples_params, samples_idx, rng_seed):
+
+    skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=rng_seed)
+
+    # Indexes of the picked threshold-cuts
+    smpl_idx = [cuts.index(i) for i in smpl_cuts]    
+    
+    # Corresponding index to each threshold-cut
+    samples_filtered = filter_sampling(complexity_grouped, cuts=smpl_cuts, p=1, random_state=1234)
+    
+    for i, complexity_index in list(zip(smpl_idx, samples_filtered)):
+        
+        best_score = 0
+        best_params = None
+        for params in list(ParameterGrid(grid_params)):
+            
+            clf = method(**params)
+            scores = []
+            
+            for train_index, test_index in skf.split(X_train, y_train):
+                
+                try:
+                    sample_index = list(set(train_index).intersection(complexity_index))
+                    clf.fit(X_train[sample_index], y_train[sample_index])
+                    preds = clf.predict(X_train[test_index])
+                    scoring = scaled_mcc(y_train[test_index], preds)
+                except:
+                    scoring = 0
+                scores.extend([scoring])
+                
+            if np.mean(scores) > best_score:
+                best_score = np.mean(scores)
+                best_params = params
+                
+        samples_scores[i] = best_score
+        samples_params[i] = best_params
+        samples_idx[i] = complexity_index
+        
+    return samples_scores, samples_params, samples_idx
+
+
+def search_idx(samples_scores, samples_params, cuts):
+    
+    best_index = np.argmax(samples_scores)
+    righ_index = best_index+1
+    left_index = best_index-1
+    new_index = []
+    
+    if left_index >= 0:
+        if samples_params[left_index] is None:
+            new_index.extend([left_index])
+    if righ_index < len(cuts):
+        if samples_params[righ_index] is None:
+            new_index.extend([righ_index])
+            
+    return new_index
+
+def minority_class_properties(y_train):
+    mask_class_0 = y_train == 0
+    mask_class_1 = y_train == 1
+    
+    if mask_class_1.mean()<0.50:
+        minority_class_idx = np.where(mask_class_1)[0]
+        minority_class_proportion = mask_class_1.sum()/len(y_train)
+    else:
+        minority_class_idx = np.where(mask_class_0)[0]
+        minority_class_proportion = mask_class_0.sum()/len(y_train)
+    
+    return minority_class_idx, minority_class_proportion
